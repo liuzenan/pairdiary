eeac222714d563c9e4992b0e01809dfcbe8e6251
Song Qiyue <harrisonsong1991@gmail.com>
2014-02-15 15:57:21 +0800

    add Facebook login handler

--- PairDiary/PairDiaryAppDelegate.m
+++ PairDiary/PairDiaryAppDelegate.m
@@ -16,7 +16,8 @@
     // Override point for customization after application launch.
     [Parse setApplicationId:@"ZvzmSjqQC94Cigneyvqid3dk9nr0g6ytaq84Wzcr"
                   clientKey:@"V5onbrCWWt4w73y3q7E19ZHCQmKrVNnp1Ml9ivT5"];
-    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions]; 
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    [PFFacebookUtils initializeFacebook];
     return YES;
 }
 							
@@ -40,11 +41,21 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [FBAppCall handleDidBecomeActiveWithSession:[PFFacebookUtils session]];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+-(BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    return [FBAppCall handleOpenURL:url
+                  sourceApplication:sourceApplication
+                        withSession:[PFFacebookUtils session]];
 }
 
 @end
