dd7da0f3ed0e07ceab0afdc7aae8191f52bdc56b
Shao <shaohuan.li@gmail.com>
2014-02-15 23:46:21 +0800

    Added PairDiaryMessage Back

---
+++ PairDiary/Controllers/PairDiaryMessageViewController.h
@@ -0,0 +1,16 @@
+//
+//  PairDiaryMessageViewController.h
+//  PairDiary
+//
+//  Created by Liu Zenan on 15/2/14.
+//  Copyright (c) 2014 li.shaohuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+#import <JSMessagesViewController/JSMessagesViewController.h>
+
+@interface PairDiaryMessageViewController : JSMessagesViewController <JSMessagesViewDataSource, JSMessagesViewDelegate>
+@property (strong, nonatomic) PFObject *pair;
+//@property (strong, nonatomic) PFUser *withUser;
+@end
