e1cc103b3a1694d0c2dc6a8266a1cdae21147200
Liu Zenan <liuzenan711@gmail.com>
2014-02-15 23:32:38 +0800

    add user

--- PairDiary/PairDiaryAppDelegate.m
+++ PairDiary/PairDiaryAppDelegate.m
@@ -44,7 +44,6 @@
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     if (![[UserController sharedInstance] isLoggedIn]) {
-        NSLog(@"login already.");
         UIStoryboard *storyBoard = [UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]];
         LoginViewController * PairDiaryLogin = (LoginViewController *)[storyBoard instantiateViewControllerWithIdentifier:@"Login"];
         [self.window.rootViewController presentViewController:PairDiaryLogin animated:NO completion:Nil];
